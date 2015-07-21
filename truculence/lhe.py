2a4ca267c5675e880e1660836ae8cfe9c58e700d
Tote <tote@physics.rutgers.edu>
2015-07-21 08:07:51 -0500

    150721: Adding LHE scripts

--- truculence/lhe.py
+++ truculence/lhe.py
@@ -1,53 +1,69 @@
-def parse_lhe(f="", verbose=False):
-	if (verbose): print "> Parsing the LHE file at {0}".format(f)
-	
-	# Open and read the LHE file:
-	raw = open(f, "r").read().split("<event>")
-	meta = raw[0]
-	raw = raw[1:]		# The first element is the metadata, so I ignore it.
+from re import search
+import os
 
-	if (verbose): print "This LHE file contains {} events.".format(len(raw_in)-1)
-	count = -1
+def get_info(f=""):
+#	labels = ["event", "init", "header", "MGVersion", "MG5ProcCard", "MGProcCard", "MGRunCard", "slha", "MGGenerationInfo", "other"]
+	n = {}
+	with open(f) as file_in:		# This won't load the entire file into memory, which is important since LHE files can be enormous.
+		for line in file_in:
+			match_start = search("<(\w+)\s?.*>", line)
+			match_end = search("</(\w+)\s?.*>", line)
+			if match_start:
+				label = match_start.group(1)
+#				if label != "event":
+#					print label
+				if label not in n:
+					n[label] = [0]*2
+				n[label][0] += 1
+			elif match_end:
+				label = match_end.group(1)
+				n[label][1] += 1
+	for label, count in n.iteritems():
+		print "{0}: {1}".format(label, count)
+
+def simplify(f="", o="simple.lhe"):		# This function removes metadata from an LHE file.
+	# Delete the output file if it already exists:
+	if os.path.exists(o):
+		os.remove(o)
+	
+	# Simplify the input file:
 	n = 0
-	save = 0
-	for event in raw:
-		count += 1
-	#	if count < 4:
-	#		print "=========="
-	#		print event.strip()
-	#		print "=========="
-		lines = event.strip().split("\n")
-#		particles = []
-#		meta = []
-#		protons = []
-#		for line in lines:
-#	#		print "> {0}".format(len(line.split()))
-#			particles.append(line) if len(line.split()) == 13 else meta.append(line)		# Get the particle lines from the event.
-#		if count == 10:
-#	#		print particles
-#			for particle in particles:
-#				print particle
-#			print meta
-#		pt_squarks = []
-#		for particle in particles:
-#			variables = particle.split()
-#			pdgid = int(variables[0])
-#			mass = float(variables[10])
-#			p = []
-#			p.append(float(variables[6]))
-#			p.append(float(variables[7]))
-#			p.append(float(variables[8]))
-#			e = float(variables[9])
-#			pt = (p[0]**2 + p[1]**2)**(0.5)
-#			if count == 3:
-#				print "> {0}".format(pt)
-#			if (abs(pdgid) == 1000005):
-#				pt_squarks.append(pt)
-#				if (pt >= cut_pt):
-#					save = 1		# Both squarks should have the same pT...
-#		h1_min.Fill(min(pt_squarks))
-#		h1_max.Fill(max(pt_squarks))
-#		if save:
-#			n += 1
-#			raw_out += "<event>" + thing
-#			save = 0
+	z_inside = False
+	with open(o, "a") as file_out:
+		with open(f) as file_in:		# This won't load the entire file into memory, which is important since LHE files can be enormous.
+			for line in file_in:
+				n += 1
+#				if n < 50:
+#					print z_inside
+				if "<header>" in line:
+					z_inside = True
+				elif "</header>" in line:
+					z_inside = False
+				elif not z_inside:
+					file_out.write(line)
+
+def change_process_code(f="", o="", code=9999):		# This is extremely experimental, since I have no idea what this number means nor where else it's repeated.
+	if f:
+		if not o:
+			o = f[:-4] + "_modified.lhe"
+		if os.path.exists(o):
+			os.remove(o)
+		
+		with open(o, "a") as file_out:
+			with open(f) as file_in:		# This won't load the entire file into memory, which is important since LHE files can be enormous.
+				nevent = 0
+				z_inside = False
+				for line in file_in:
+					if "<event>" in line:
+						z_inside = True
+						nevent += 1
+						if nevent % 20000 == 0:
+							print nevent
+					elif "</event>" in line:
+						z_inside = False
+					if z_inside and len(line.strip().split()) == 6:
+						line_modified = line.strip().split()
+						line_modified[1] = str(code)
+						file_out.write(" " + "  ".join(line_modified) + "\n")
+					else:
+						file_out.write(line)
